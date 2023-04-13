fd9128d61770b0efd7003da2615e245f1da24399
JinWon <dlfjs9514@nate.com>
2023-04-13 16:16:43 +0900

    tailwind - response break 구간 설정

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+	mode: "jit",
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "1025px",
+			xl: "1280px",
+			xl2: "1360px",
+		},
+		extend: {},
+	},
+	plugins: [require("daisyui")],
 };
