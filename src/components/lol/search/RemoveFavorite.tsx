d557a62148b16f6c295000fa285b0f717824b521
JinWon <dlfjs9514@nate.com>
2023-04-13 16:24:04 +0900

    즐겨찾기 삭제 버튼 추가

---
+++ src/components/lol/search/RemoveFavorite.tsx
@@ -0,0 +1,31 @@
+import { updateDoc, doc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { db } from "../../../api/firebase";
+import styled from "styled-components";
+import { auth } from "../../../api/firebase";
+interface IUserName {
+	userName: string;
+}
+const RemoveFavorite = (props: IUserName) => {
+	const frankDocRef = doc(db, "LolUser", `${auth.currentUser?.email}`);
+	console.log(props);
+	const remove = async () => {
+		// 즐겨찾기 삭제
+		await updateDoc(frankDocRef, {
+			favorite: arrayRemove(props.userName),
+		});
+	};
+	return <RemoveBtn onClick={remove}>x</RemoveBtn>;
+};
+
+const RemoveBtn = styled.button`
+	padding: 0;
+	padding: 5px 10px;
+	height: 30px;
+	border: none;
+	line-height: 1;
+	& :focus {
+		outline: none;
+	}
+`;
+
+export default RemoveFavorite;
