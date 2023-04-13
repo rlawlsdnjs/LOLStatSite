c18d9cab18d6f55533105daa458110f3fbc595a6
JinWon <dlfjs9514@nate.com>
2023-04-13 16:22:20 +0900

    no message

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,9 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -21,3 +18,6 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+
+
+// 필요한 곳에서 사용할 수 있도록 내보내기
