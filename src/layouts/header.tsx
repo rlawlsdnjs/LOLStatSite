299de9767a0563b5cc50e281e8b34e4be9445178
JinWon <dlfjs9514@nate.com>
2023-04-13 16:25:41 +0900

    header 닉네임 추가 및 코드정리

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,52 +1,61 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
-
-import { loginState } from "../store";
+import { useRecoilState } from "recoil";
+import { loginState } from "../store/lol";
 import { AuthContext } from "../components/sign/context/authContext";
 import { useContext } from "react";
-import { handleLogout } from "../components/sign/sign";
-
+import { SignUp } from "../components/sign/sign";
+import { signOut } from "firebase/auth";
+import { auth } from "../api/firebase";
+import Logo from "../../public/assets/images/logo.png";
 const Header = () => {
-  const userInfo = useContext(AuthContext);
-  const [sign, setSign] = useRecoilState<boolean>(loginState);
-  const signIn = () => {
-    setSign((current) => !current);
-  };
+	const userInfo = useContext(AuthContext);
+	const [sign, setSign] = useRecoilState<boolean>(loginState);
+	const user = auth.currentUser;
+	const signIn = () => {
+		setSign((current) => !current);
+	};
 
-  return (
-    <HeaderContainer>
-      <HeaderCenter>
-        <HeaderTitle>
-          <Link to="/">
-            <LogoImg src="test.png" />
-          </Link>
-        </HeaderTitle>
-        <SignList className="signList">
-          <li>
-            {!userInfo ? (
-              <button onClick={signIn}>로그인</button>
-            ) : (
-              <button onClick={handleLogout}>로그아웃</button>
-            )}
-          </li>
-        </SignList>
-      </HeaderCenter>
-    </HeaderContainer>
-  );
+	const handleLogout = () => {
+		signOut(auth);
+		setSign(false);
+	};
+
+	return (
+		<HeaderContainer>
+			<HeaderCenter>
+				<HeaderTitle>
+					<Link to="/">
+						<LogoImg src={Logo} alt="메인 Logo" />
+					</Link>
+				</HeaderTitle>
+				<SignList className="signList">
+					{userInfo && (
+						<li>
+							<span>{user?.displayName}</span>님
+						</li>
+					)}
+					<li>
+						{!userInfo ? (
+							<button onClick={signIn}>Login</button>
+						) : (
+							<button onClick={handleLogout}>LogOut</button>
+						)}
+					</li>
+				</SignList>
+			</HeaderCenter>
+		</HeaderContainer>
+	);
 };
 const HeaderContainer = styled.div`
-  position: fixed;
-  width: 100%;
-  top: 0;
-  left: 0;
+	position: fixed;
+	width: 100%;
+	max-width: 1280px;
+	margin: 0 auto;
+	top: 0;
+	left: 50%;
+	transform: translateX(-50%);
+	padding: 20px;
 `;
 const HeaderCenter = styled.div`
     display: flex;
@@ -56,15 +65,45 @@ const HeaderCenter = styled.div`
 `;
 
 const HeaderTitle = styled.h1`
-  margin-right: auto;
+	margin-right: auto;
 `;
 const LogoImg = styled.img`
-  max-width: 30px;
+	max-width: 50px;
 `;
 const SignList = styled.ul`
-  margin-left: auto;
+	margin-left: auto;
+	align-items: center;
+	display: flex;
+	& button {
+		appearance: none;
+		background: none;
+		color: #fff;
+		border: none;
 
-  display: flex;
+		font-weight: bold;
+		border-radius: 0 !important;
+
+		font-size: 20px;
+		padding: 0;
+	}
+
+	& button:focus {
+		outline: none !important;
+	}
+
+	& li {
+		margin-left: 2rem;
+		border-bottom: 1px solid #fff;
+		padding: 15px 0;
+	}
+	& li:first-child span {
+		font-size: 25px;
+		color: #fff;
+	}
+	& li:first-child {
+		color: #fff;
+		font-size: 20px;
+	}
 `;
 
 export default Header;
