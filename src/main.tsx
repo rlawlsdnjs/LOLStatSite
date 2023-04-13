ef5e0464e8fba39519aebe531119632eb7f22203
JinWon <dlfjs9514@nate.com>
2023-04-13 16:21:11 +0900

    swiper 초기 css 변경

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import AuthProvider from "./components/sign/provider/authProvider";
 import "./reset.css";
-
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+	<React.StrictMode>
+		<RecoilRoot>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</RecoilRoot>
+	</React.StrictMode>
 );
