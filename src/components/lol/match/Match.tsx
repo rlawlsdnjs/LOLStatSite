3c07c2f8954ed326e0fddb7cd8b6550a87767cea
JinWon <dlfjs9514@nate.com>
2023-04-14 21:44:39 +0900

    no message

--- src/components/lol/match/Match.tsx
+++ src/components/lol/match/Match.tsx
@@ -28,14 +28,14 @@ const UserMatch = (props: any) => {
 				{props.matchL?.map((match: any | object) => {
 					// 검색 유저 정보
 
-					const searchUser = match?.data?.info?.participants?.filter(
+					const searchUser = match.data.info.participants.filter(
 						(user: any) => user.summonerName == currentSearchKey
 					);
 
-					const blueTeam = match?.data?.info?.participants?.filter(
+					const blueTeam = match.data.info.participants.filter(
 						(user: any) => user.teamId == 100
 					);
-					const purpleTeam = match?.data?.info?.participants?.filter(
+					const purpleTeam = match.data.info.participants.filter(
 						(user: any) => user.teamId == 200
 					);
 					console.log(searchUser);
