673b80f74b9da69d62c887f33226bb49f6d871d9
JinWon <dlfjs9514@nate.com>
2023-04-28 18:20:46 +0900

    no message

--- api/lolDetailMatch.ts
+++ api/lolDetailMatch.ts
@@ -1,5 +1,4 @@
 
-
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
@@ -10,13 +9,25 @@ const KEY = `api_key=${process.env.VITE_RIOT_API_KEY}`;
 export default async function getDetailMatch(request: VercelRequest, response: VercelResponse) {
   try {
     const { gameIds } = request.body;
- 
+    
     const promises = gameIds.map((gameId: string) =>
-      fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/${gameId}?${KEY}`)
+       fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/${gameId}?${KEY}`)
         .then((res) => res.json())
     );
 
-    const results = await Promise.all(promises);
+    const matchArr: any = [];
+
+    for (let i = 0, len = gameIds.length; i < len; i++) {
+      matchArr.push(
+        fetch(`/api/lol/match/v5/matches/${gameIds[i]}`, {
+          headers: {
+            "X-Riot-Token": KEY,
+          },
+        })
+      );
+    }
+
+    const results = await Promise.all(matchArr);
 
     response.status(200).json({ results });
   } catch (err) {
