9baf78fc6759fde3438c6117ab09a1b03004481d
JinWon <dlfjs9514@nate.com>
2023-04-28 20:36:06 +0900

    lol user match list 함수 수정

--- api/lolUserMatch.ts
+++ api/lolUserMatch.ts
@@ -10,13 +10,13 @@ export default async function getUserMatch(request: VercelRequest, response: Ver
   try {
 
     const { body } = request;
-    const payload = body.data;
-    const { data } = payload;
-    const res = await fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/by-puuid/${payload}/ids?start=0&count=20&${KEY}`);
+    const matchId = body.data;
+    const { data } = matchId;
+    const res = await fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/by-puuid/${matchId}/ids?start=0&count=20&${KEY}`);
     const result = await res.json().then((data) => data);
 
 
-    const matchDetail =     result.map((gameId: string) =>
+    const matchDetail = result.map((gameId: string) =>
     fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/${gameId}?${KEY}`)
      .then((res) => res.json()));
 
