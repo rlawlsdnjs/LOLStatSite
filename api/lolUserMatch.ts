8e8e8231c9c39402e4b3a19a94d8a4411e6b921f
JinWon <dlfjs9514@nate.com>
2023-04-28 20:29:47 +0900

    no message

--- api/lolUserMatch.ts
+++ api/lolUserMatch.ts
@@ -15,8 +15,14 @@ export default async function getUserMatch(request: VercelRequest, response: Ver
     const res = await fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/by-puuid/${payload}/ids?start=0&count=20&${KEY}`);
     const result = await res.json().then((data) => data);
 
+
+    const matchDetail =     result.map((gameId: string) =>
+    fetch(`${URL_ASIA_RIOT}/lol/match/v5/matches/${gameId}?${KEY}`)
+     .then((res) => res.json()));
+
+     const matchData = await Promise.all(matchDetail).then((data) => data);;
     response.status(200).json({
-      result,
+      matchData,
     });
   } catch (err) {
     console.log(err);
