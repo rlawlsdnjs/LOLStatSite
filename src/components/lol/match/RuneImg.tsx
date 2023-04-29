ca92e1589dac46437785f2dea22b564723281303
JinWon <dlfjs9514@nate.com>
2023-04-29 14:27:57 +0900

    룬 이미지 절대경로 수정

--- src/components/lol/match/RuneImg.tsx
+++ src/components/lol/match/RuneImg.tsx
@@ -20,15 +20,13 @@ const RuneImg = (runes: any) => {
 				<div className="w-16 rounded">
 					<img
 						className="icon"
-						src={`../../../../public/assets/images/${rune01[0].icon}`}
+						src={`/public/assets/images/${rune01[0].icon}`}
 					></img>
 				</div>
 			</Rune>
 			<Rune>
 				<div className="w-16 rounded">
-					<img
-						src={`../../../../public/assets/images/${runesStyle02[0].icon}`}
-					></img>
+					<img src={`/public/assets/images/${runesStyle02[0].icon}`}></img>
 				</div>
 			</Rune>
 		</>
