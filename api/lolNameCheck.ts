54676ad84649fc76bbabe7f78b3c87242f4ff653
JinWon <dlfjs9514@nate.com>
2023-05-07 21:43:23 +0900

    소환사명 오류 수정 확인

--- api/lolNameCheck.ts
+++ api/lolNameCheck.ts
@@ -3,7 +3,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
-const URL_ASIA_RIOT = 'https://asia.api.riotgames.com';
+const URL_KR_RIOT = 'https://kr.api.riotgames.com';
 
 const KEY = `api_key=${process.env.VITE_RIOT_API_KEY}`;
 export default async function getUserName(request: VercelRequest, response: VercelResponse) {
@@ -11,12 +11,16 @@ export default async function getUserName(request: VercelRequest, response: Verc
 
     const { body } = request;
     const summonerName = body.data;
-    const res = await fetch(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?${KEY}`);
-    if (res.status === 404) {
-        return { check: false };
-      }
-      const data = await res.json();
-      return { check: true };
+    const res = await fetch(`${URL_KR_RIOT}/lol/summoner/v4/summoners/by-name/${summonerName}?${KEY}`);
+    if ( response.status(200)) {
+        if (res.status === 404) {
+            return { check: false };
+          }
+          const data = await res.json();
+          
+          return { check: true };
+    }
+
   } catch (err) {
     return { check: false };
   }
