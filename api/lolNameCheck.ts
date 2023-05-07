173d3aa54e92234c5711c3779dd910a8c8ab4cf1
JinWon <dlfjs9514@nate.com>
2023-05-07 21:47:14 +0900

    소환사명 유무 오류 확인

--- api/lolNameCheck.ts
+++ api/lolNameCheck.ts
@@ -7,21 +7,21 @@ const URL_KR_RIOT = 'https://kr.api.riotgames.com';
 
 const KEY = `api_key=${process.env.VITE_RIOT_API_KEY}`;
 export default async function getUserName(request: VercelRequest, response: VercelResponse) {
-  try {
-
-    const { body } = request;
-    const summonerName = body.data;
-    const res = await fetch(`${URL_KR_RIOT}/lol/summoner/v4/summoners/by-name/${summonerName}?${KEY}`);
-    if ( response.status(200)) {
-        if (res.status === 404) {
-            return { check: false };
-          }
-          const data = await res.json();
-          
-          return { check: true };
+    try {
+  
+      const { body } = request;
+      const summonerName = body.data;
+      const res = await fetch(`${URL_KR_RIOT}/lol/summoner/v4/summoners/by-name/${summonerName}?${KEY}`);
+      if (response.statusCode === 200) {
+          if (res.status === 404) {
+              return { check: false };
+            }
+            const data = await res.json();
+            
+            return { check: true };
+      }
+  
+    } catch (err) {
+      return { check: false };
     }
-
-  } catch (err) {
-    return { check: false };
   }
-}
